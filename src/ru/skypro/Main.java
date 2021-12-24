2cc61aa47248528576d1b24507b4cbd1f8e20ec5
Никита <oleinik.nikita96@gmail.com>
2021-12-24 22:02:20 +0300

    Исправил по вашим комментариям переменные, добавил в гитигнор /.idea/ и *.iml, надеюсь больше эти файлы не будут загружаться в гит.

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -1,14 +1,12 @@
 package ru.skypro;
-
 public class Main {
-
-    public static void main(String[] args) {
-	byte a = 1;
-    short b = 30000;
-    int c = -1967;
-    long d = 98776213L;
-    float e = 9.076f;
-    double f = -3.42985;
+    public static void main ( String[] args ) {
+	byte byteVar = 1;
+    short shortVar = 30000;
+    int intVar = -1967;
+    long longVar = 98776213L;
+    float floatVar = 9.076f;
+    double doubleVar = -3.42985;
 
     double firstWeight = 78.2;
     double secondWeight = 82.7;
@@ -17,38 +15,42 @@ public class Main {
     System.out.println("Общий вес боксеров равен: " + totalWeight + " кг");
     System.out.println("Разница в весе боксеров составляет: " + diffWeight + " кг");
 
-    int bananMass = 80;
-    int molokoMass = 105;
-    int icecreamMass = 100;
-    int eggMass = 70;
-    int allMass = bananMass*5 + molokoMass*2 + icecreamMass*2 + eggMass*4;
-    float kgMass = allMass/1000F;
-    System.out.println("Вес завтрака в граммах: " + allMass);
-    System.out.println("Вес завтрака в кг: " + kgMass + " кг");
+    int bananaWeight = 80;
+    int milkWeight = 105;
+    int iceCreamWeight = 100;
+    int eggWeight = 70;
+    int bananaAmount = 5;
+    int milkAmount = 2;
+    int iceCreamAmount = 2;
+    int eggAmount = 4;
+    int allWeight = bananaWeight*bananaAmount + milkWeight*milkAmount + iceCreamWeight*iceCreamAmount + eggWeight*eggAmount;
+    float kgWeight = allWeight/1000F;
+    System.out.println("Вес завтрака в граммах: " + allWeight);
+    System.out.println("Вес завтрака в кг: " + kgWeight + " кг");
 
     int goal = 7000;
-    int lose1 = 250;
-    int lose2 = 500;
-    int time1 = goal/lose1;
-    int time2 = goal/lose2;
-    int average = (time1 +time2)/2;
-    System.out.println("Дней при первом способе похудения: "+ time1);
-    System.out.println("Дней при втором способе похудения: "+ time2);
+    int lossOne = 250;
+    int lossTwo = 500;
+    int timeOne = goal/lossOne;
+    int timeTwo = goal/lossTwo;
+    int average = (timeOne +timeTwo)/2;
+    System.out.println("Дней при первом способе похудения: "+ timeOne);
+    System.out.println("Дней при втором способе похудения: "+ timeTwo);
     System.out.println("Потребуется в среднем дней: " + average);
 
-    int zpMasha = 67760;
-    int zpDenis = 83690;
-    int zpKristina = 76230;
-    var zpMashaNew = zpMasha*1.1;
-    var zpDenisNew = zpDenis*1.1;
-    var zpKristinaNew = zpKristina*1.1;
+    int payMasha = 67760;
+    int payDenis = 83690;
+    int payKristina = 76230;
+    var payMashaNew = payMasha*1.1;
+    var payDenisNew = payDenis*1.1;
+    var payKristinaNew = payKristina*1.1;
 
-    var diffMasha = (zpMashaNew - zpMasha)*12;
-    double diffDenis = (zpDenisNew - zpDenis)*12;
-    double diffKristina = (zpKristinaNew - zpKristina)*12;
-    System.out.println("Новый доход Маши в рублях: " + zpMashaNew + ". Рост дохода в рублях за год: " + diffMasha);
-    System.out.println("Новый доход Дениса в рублях: " + zpDenisNew + ". Рост дохода в рублях за год: " + diffDenis);
-    System.out.println("Новый доход Кристины в рублях: " + zpKristinaNew + ". Рост дохода в рублях за год: " + diffKristina);
+    var diffMasha = (payMashaNew - payMasha)*12;
+    double diffDenis = (payDenisNew - payDenis)*12;
+    double diffKristina = (payKristinaNew - payKristina)*12;
+    System.out.println("Новый доход Маши в рублях: " + payMashaNew + ". Рост дохода в рублях за год: " + diffMasha);
+    System.out.println("Новый доход Дениса в рублях: " + payDenisNew + ". Рост дохода в рублях за год: " + diffDenis);
+    System.out.println("Новый доход Кристины в рублях: " + payKristinaNew + ". Рост дохода в рублях за год: " + diffKristina);
 
 
 
